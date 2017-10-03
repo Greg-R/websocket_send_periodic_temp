fe8ded6360ff891a3a0e3cadfa2362534e1f3bb3
Greg-R <soapy-smith@moose.com>
2017-10-03 08:36:13 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "mgos.h"
 #include "mgos_http_server.h"
+#include "mgos_dht.h"
 
 //  Clock update interval.
 #define INTERVAL_SECONDS 1
@@ -7,10 +8,22 @@
 time_t maintime;
 struct tm * localstruct;
 char utctime[50];
+char count;  //  Used to schedule temp and humidity updates.
 //  JSON message which is sent to web page using mg_printf_websocket_frame().
 char* timeString = "{\"messageType\":\"serverTime\",\"serverTime\":\"%s\"}";
+char* tempString = "{\"messageType\":\"temperature\",\"currentTemp\":\"%2.1f\"}";
+char* humidString = "{\"messageType\":\"humidity\",\"currentHumidity\":\"%2.1f\"}";
+struct mgos_dht * dht11;
+float tempC, humidity;
+
 //  Using defaults for http options in http response.
 struct mg_serve_http_opts http_opts = {.document_root = "."};
+
+float ctof(float tempC) {
+	float tempf;
+	tempf = tempC * 9.0/5.0 + 32;
+	return tempf;
+}
 
 //  This is the HTTP server handler.  It is also handling Websocket.
 //  Not all of the possible events are being handled.
@@ -42,9 +55,21 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data, void *us
 	case MG_EV_TIMER: {
 		maintime = time(NULL);
 		localstruct = localtime(&maintime);
+//		printf("The seconds are %d.\n", localstruct->tm_sec);
 		strftime(utctime, sizeof(utctime), "%A %B %d %T", localstruct);
+		tempC = mgos_dht_get_temp(dht11);
+//		printf("The temperature in degrees C is %2.1f.\n", ctof(tempC));
+		humidity = mgos_dht_get_humidity(dht11);
+//		printf("The humidity in percent is %2.1f.\n", humidity);
 		// printf("Time is %s.\n", utctime);
+		//  Update time every 1 second; temp and humidity every 10 seconds.
 		mg_printf_websocket_frame(nc, WEBSOCKET_OP_TEXT, timeString, utctime);
+		if (count == 10) {
+		mg_printf_websocket_frame(nc, WEBSOCKET_OP_TEXT, tempString, ctof(tempC));
+		mg_printf_websocket_frame(nc, WEBSOCKET_OP_TEXT, humidString, humidity);
+		count = 0;
+		}
+		count++;
 		//  A new timer is set.
 		mg_set_timer(nc, mg_time() + INTERVAL_SECONDS);
 		break;
@@ -55,6 +80,8 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data, void *us
 enum mgos_app_init_result mgos_app_init(void) {
 	struct mg_connection * nc;
 	void * user_data = "";
+	bool dhtInit;
+
 
 	//  Get the server handle.
 	if((nc = mgos_get_sys_http_server()) == NULL) {
@@ -64,5 +91,18 @@ enum mgos_app_init_result mgos_app_init(void) {
 	//  Bind the event handler to the HTTP server.
 	mgos_register_http_endpoint("/", ev_handler, user_data);
 
+	if((dht11 = mgos_dht_create(2, DHT22)) == NULL) {
+		puts("DHT11 handle not created");
+	}
+
+	dhtInit = mgos_dht_init();
+	if(dhtInit) {
+		puts("DHT sensor initialized.");
+	}
+
+//	tempC = mgos_dht_get_temp(dht11);
+//	printf("The initial temperature in degrees C is %2.1f.\n", tempC);
+
 	return MGOS_APP_INIT_SUCCESS;
 }
+
