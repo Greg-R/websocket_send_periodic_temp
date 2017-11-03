e258656f70033d425359cea36747f1408a5de75e
Greg-R <soapy-smith@moose.com>
2017-11-02 21:48:20 -0400

    Moving files around

--- doc/pandoc/makedoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pandoc --data-dir=. --template template.latex latex-config.yml chapter1.md chapter2.md chapter3.md wifi-config.md resources.md -f markdown -t latex -s -o esp32websocket.tex
-
